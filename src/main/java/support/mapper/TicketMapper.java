204d9f42b19cfe3a02d9c36c143819ad96b530c5
lucas <lbonfil@fi.uba.ar>
2021-07-25 16:03:00 -0300

    se agrega el calculo de fechas de vencimiento segun severidad

--- src/main/java/support/mapper/TicketMapper.java
+++ src/main/java/support/mapper/TicketMapper.java
@@ -23,7 +23,7 @@ public class TicketMapper {
         return this.mapTicketInfo(ticketUpdateRequest, ticket);
     }
 
-
+    //TODO: fixear las fechas para el update, no deberian recalcularse, sino usar las viejas.
     private Ticket mapTicketInfo(TicketRequest ticketRequest, Ticket ticket){
         ticket.setName(ticketRequest.getName());
         ticket.setClientId(ticketRequest.getClientId());
@@ -31,7 +31,7 @@ public class TicketMapper {
         ticket.setSeverity(ticketRequest.getSeverity());
         ticket.setCreatorName(ticketRequest.getCreator());
         ticket.setCreationDate(LocalDateTime.now());
-        ticket.setExpirationDate(LocalDateTime.now());
+        //ticket.setExpirationDate(LocalDateTime.now());
         ticket.setProductId(ticketRequest.getProductId());
         ticket.setState(ticketRequest.getState());
         ticket.setPriority(ticketRequest.getPriority());
@@ -42,7 +42,28 @@ public class TicketMapper {
         }else{
             ticket.setResolvedDate(null);
         }
+        ticket.setExpirationDate(this.calculateExpirationDate(ticketRequest.getSeverity(), LocalDateTime.now()));
 
         return ticket;
+    }
+
+    private LocalDateTime calculateExpirationDate(Severity severity, LocalDateTime date){
+        LocalDateTime new_date;
+        if (severity.equals(Severity.S1)){
+            new_date = date.plusDays(7);
+        }
+        else if (severity.equals(Severity.S2)){
+            new_date = date.plusDays(30);
+        }
+        else if (severity.equals(Severity.S3)){
+            new_date = date.plusDays(90);
+        }
+        else if (severity.equals(Severity.S4)){
+            new_date = date.plusDays(365);
+        }
+        else {
+            new_date = date;
+        }
+        return new_date;
     }
 }
